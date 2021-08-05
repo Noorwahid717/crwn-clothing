e5e51ec11aa1d3c7c94952ad4bace136134b52c6
Noorwahid717 <noorwahid052002@gmail.com>
2021-08-05 14:36:31 +0700

    menu-item updated again

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import { Switch, Route } from 'react-router-dom';
@@ -6,7 +7,8 @@ const HatsPage = () => (
     <div>
         <h1>hats page</h1>
     </div>
-)
+);
+
 function App() {
   return (
     <div>
