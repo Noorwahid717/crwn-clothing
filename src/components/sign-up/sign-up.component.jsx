e39b9f613b26f03b8202150947cd3d05321eef12
Noorwahid717 <noorwahid052002@gmail.com>
2021-08-07 00:04:36 +0700

    finishing sign in handleSubmit method

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -22,7 +22,7 @@ class SignUp extends React.Component {
     const { displayName, email, password, confirmPassword } = this.state;
 
     if (password !== confirmPassword) {
-      alert("passwords don't match");
+      alert("password tidak sama!");
       return;
     }
 
