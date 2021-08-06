713c64bafd235f038580e18752b0edc34dcb5b0a
Noorwahid717 <noorwahid052002@gmail.com>
2021-08-06 23:30:46 +0700

    replace src and public

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './custom-button.styles.scss';
+
+import './custom-buttom.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <div className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
-        {children}
-    </div>
-)
+  <button
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
 
 export default CustomButton;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.styles.scss';
-import { ReactComponent as Logo  } from '../../assets/pegasus.svg';
+
 import { auth } from '../../firebase/firebase.utils';
 
-const Header = ({ currentUser }) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                Belanja
-            </Link>
-        
-            <Link className='option' to='/shop'>
-                Hubungi
-            </Link>
+import { ReactComponent as Logo } from '../../assets/pegasus.svg';
 
-        {currentUser ? (
-                <div className='option' onClick={() => auth.signOut()}> Keluar </div>
-            ) : (
-                <Link className='option' to='signin'> Masuk </Link>
-            )
-        }
+import './header.styles.scss';
+
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        Belanja
+      </Link>
+      <Link className='option' to='/shop'>
+        Hubungi
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          Keluar
         </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          Masuk
+        </Link>
+      )}
     </div>
-)
+  </div>
+);
 
 export default Header;
